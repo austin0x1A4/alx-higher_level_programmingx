3747ace6e2ed58ad204160876289776f9f7b7b3e
austin0x1A4 <karurimm@gmail.com>
2023-10-31 23:11:02 -0700

    task 2:Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,16 +1,24 @@
+#!/usr/bin/python3
+"""Prints 'My name is <first name> <last name>'
+
+Prototype: def say_my_name(first_name, last_name=""):
+- first_name and last_name must be strings otherwise, raise a TypeError 
+exception with the message first_name must be a string or last_name must be a string
+- You are not allowed to import any module
+"""
+
 def say_my_name(first_name, last_name=""):
-    """
-    Prints "My name is <first name> <last name>".
+    """Prints 'My name is <first name> <last name>'
 
-    Parameters:
-    first_name (str): The first name.
-    last_name (str, optional): The last name. Default is an empty string.
-
+    Args:
+        first_name: first name string
+        last_name: last name string, default empty
+    
     Raises:
-    TypeError: If first_name or last_name is not a string.
+        TypeError: if first_name or last_name are not strings 
     """
-    if not isinstance(first_name, str) or not isinstance(last_name, str):
-        raise TypeError("first_name must be a string or last_name must be a string")
-
-    full_name = first_name + " " + last_name if last_name else first_name
-    print("My name is " + full_name)
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
