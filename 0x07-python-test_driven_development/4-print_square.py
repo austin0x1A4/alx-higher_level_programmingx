5f4cabe36afa4ec87569e795f0b368bd2d857fbc
austin0x1A4 <karurimm@gmail.com>
2023-10-31 23:19:33 -0700

    task 3: Print Square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
+"""Prints a square with the # character. 
+
+Prototype: def print_square(size):
+- size is the size length of the square
+- size must be an integer, otherwise raise a TypeError exception with the message 
+  size must be an integer
+- if size is less than 0, raise a ValueError exception with the message 
+  size must be >= 0 
+- if size is a float and is less than 0, raise a TypeError
+- You are not allowed to import any module
+"""
 
 def print_square(size):
-    """
-    Prints a square with the character #.
+    """Print a square with the # character.
 
-    Parameters:
-    size (int): The size length of the square.
+    Args:
+        size: length of the square
 
     Raises:
-    TypeError: If size is not an integer.
-    ValueError: If size is less than 0.
+        TypeError: if size is not an integer 
+        ValueError: if size is less than 0
     """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-
-    for _ in range(size):
+    
+    for i in range(size):
         print("#" * size)
-
-if __name__ == "__main__":
-    try:
-        print_square(4)
-        print("\n")
-        print_square(10)
-        print("\n")
-        print_square(0)
-        print("\n")
-        print_square(1)
-        print("\n")
-        print_square(-1)
-    except Exception as e:
-        print(e)
