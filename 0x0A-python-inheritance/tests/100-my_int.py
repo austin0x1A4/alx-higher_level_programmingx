82576cbf6376b966595ae22f44806d1aba8d7a8a
austin0x1A4 <karurimm@gmail.com>
2023-11-06 18:01:10 -0800

    Python inheritance ass.

---
+++ 0x0A-python-inheritance/tests/100-my_int.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class MyInt(int):
+    def __eq__(self, other):
+        """Overrides the == operator."""
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """Overrides the != operator."""
+        return super().__eq__(other)
---
+++ 0x0A-python-inheritance/tests/9-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+class BaseGeometry:
+    def area(self):
+        """Raises an Exception with the message 'area() is not implemented'."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates that value is an integer and greater than 0, or raises exceptions."""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Calculates and returns the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns a string representation of the rectangle."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
